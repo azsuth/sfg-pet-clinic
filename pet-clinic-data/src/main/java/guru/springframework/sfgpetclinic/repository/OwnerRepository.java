9780c3c277be0ca3cc6db494ad49b3d337eb0184
Andrew Sutherland <azsuth@gmail.com>
2019-04-12 09:46:46 -0500

    added jpa owner service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/OwnerRepository.java
@@ -3,5 +3,9 @@ package guru.springframework.sfgpetclinic.repository;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Optional<Owner> findByLastName(String lastName);
 }
