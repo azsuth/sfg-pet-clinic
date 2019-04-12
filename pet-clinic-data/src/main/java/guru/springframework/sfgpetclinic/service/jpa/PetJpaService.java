d5b77ed3286a79730aaf7f42ee07f6c99f60c802
Andrew Sutherland <azsuth@gmail.com>
2019-04-12 10:02:36 -0500

    added jpa services for all the entities

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/jpa/PetJpaService.java
@@ -0,0 +1,51 @@
+package guru.springframework.sfgpetclinic.service.jpa;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repository.PetRepository;
+import guru.springframework.sfgpetclinic.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    @Autowired
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/jpa/VetJpaService.java
@@ -0,0 +1,51 @@
+package guru.springframework.sfgpetclinic.service.jpa;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.repository.VetRepository;
+import guru.springframework.sfgpetclinic.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    @Autowired
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
